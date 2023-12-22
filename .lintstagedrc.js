65dccc85eebd1764a748aa06483d4d1b5082590c
Amin Partovi <amin.frontend@gmail.com>
2023-12-22 15:03:56 +0330

    build: add dependencies and configs related to code quality and formatting

---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "*.ts": ["prettier --write --cache", "eslint --cache"],
+    "*.tsx": ["prettier --write --cache", "eslint --cache"],
+    "*.css": "prettier --write --cache",
+  };
